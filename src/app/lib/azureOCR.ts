78602fa47e11f2281c782ec9e357f620f375f5a6
Harikrishna C <harikrishna.cvv230149@cvv.ac.in>
2025-03-08 22:59:24 +0530

    Changes to azureOCR

--- src/app/lib/azureOCR.ts
+++ src/app/lib/azureOCR.ts
@@ -1,22 +1,4 @@
-import Cors from "cors";
 import { AzureKeyCredential, DocumentAnalysisClient } from "@azure/ai-form-recognizer";
-import { NextApiRequest, NextApiResponse } from "next";
-
-const cors = Cors({
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-  });
-  
-  function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-    return new Promise((resolve, reject) => {
-      fn(req, res, (result: any) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-        return resolve(result);
-      });
-    });
-  }
 
 const AZURE_OCR_ENDPOINT = "https://ai-medical.cognitiveservices.azure.com/";
 const AZURE_OCR_KEY = "EUgJOXnmZw6Y3Pg4ExeQYb3q4TOyLECvhkeACxexeaW3l7NoeXBTJQQJ99BCACYeBjFXJ3w3AAALACOGEqQj";
